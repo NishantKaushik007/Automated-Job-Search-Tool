1c2606f1f67ec0499ce2af6305ae75410efc0bc5
NishantKaushik007 <nishant.nishantkaushik.nishant@gmail.com>
2023-07-01 00:17:45 +0530

    More Companies Added

--- companyProcessing.py
+++ companyProcessing.py
@@ -26,15 +26,15 @@ def initComp(tokenURL,start,end,jobURL,payload):
         jobsDictionary.append(Job)
     return jobsDictionary
 
-def writeToExcel(title,jobsDictionary,worksheet):
+def writeToExcel(title,jobsDictionary,worksheet,cell_format):
     global row
     row += 2
-    worksheet.write(row, 0, title)
+    worksheet.write(row, 0, title,cell_format)
     row += 1
-    worksheet.write(row, 0, 'jobTitle')
-    worksheet.write(row, 1, 'jobId')
-    worksheet.write(row, 2, 'location')
-    worksheet.write(row, 3, 'postedOn')
+    worksheet.write(row, 0, 'jobTitle',cell_format)
+    worksheet.write(row, 1, 'jobId',cell_format)
+    worksheet.write(row, 2, 'location',cell_format)
+    worksheet.write(row, 3, 'postedOn',cell_format)
     row += 1
     # iterating through content list
     for item in jobsDictionary:
@@ -47,7 +47,7 @@ def writeToExcel(title,jobsDictionary,worksheet):
         # with each iterations.
         row += 1
 
-def qualcommComp(lock,worksheet):
+def qualcommComp(lock,worksheet,cell_format):
     payload = {
         "appliedFacets": {},
         "limit": 10,
@@ -55,10 +55,10 @@ def qualcommComp(lock,worksheet):
     }
     jobsDictionary = initComp(tokenURL='https://qualcomm.wd5.myworkdayjobs.com/External',start=494,end=530,jobURL='https://qualcomm.wd5.myworkdayjobs.com/wday/cxs/qualcomm/External/jobs',payload=payload)
     lock.acquire()
-    writeToExcel('Qualcomm Company', jobsDictionary, worksheet)
+    writeToExcel('Qualcomm Company', jobsDictionary, worksheet,cell_format)
     lock.release()
 
-def cienaComp(lock,worksheet):
+def cienaComp(lock,worksheet,cell_format):
     payload = {
         "appliedFacets": {},
         "limit": 10,
@@ -66,14 +66,62 @@ def cienaComp(lock,worksheet):
     }
     jobsDictionary = initComp('https://ciena.wd5.myworkdayjobs.com/en-US/Careers',672,708,'https://ciena.wd5.myworkdayjobs.com/wday/cxs/ciena/Careers/jobs',payload)
     lock.acquire()
-    writeToExcel('Ciena Company',jobsDictionary,worksheet)
+    writeToExcel('Ciena Company',jobsDictionary,worksheet,cell_format)
     lock.release()
 
 
-def adobeComp(lock,worksheet):
+def adobeComp(lock,worksheet,cell_format):
     payload = {"appliedFacets":{"jobFamilyGroup":["591af8b812fa10737af39db3d96eed9f"],"locationCountry":["c4f78be1a8f14da0ab49ce1162348a5e"],"locations":["3ba4ecdf4893100bc84aa7a3f1e46b98","3ba4ecdf4893100bc84a8e6d38d46b70"]},"limit":10,"offset":0,"searchText":""}
     jobsDictionary = initComp('https://adobe.wd5.myworkdayjobs.com/external_experienced?jobFamilyGroup=591af8b812fa10737af39db3d96eed9f&locationCountry=c4f78be1a8f14da0ab49ce1162348a5e&locations=3ba4ecdf4893100bc84aa7a3f1e46b98&locations=3ba4ecdf4893100bc84a8e6d38d46b70', 504, 540,
                     'https://adobe.wd5.myworkdayjobs.com/wday/cxs/adobe/external_experienced/jobs',payload)
     lock.acquire()
-    writeToExcel('Adobe Company', jobsDictionary, worksheet)
+    writeToExcel('Adobe Company', jobsDictionary, worksheet,cell_format)
+    lock.release()
+
+def paypalComp(lock,worksheet,cell_format):
+    payload = {"appliedFacets":{},"limit":10,"offset":0,"searchText":""}
+    jobsDictionary = initComp(
+        'https://paypal.wd1.myworkdayjobs.com/jobs',
+        466, 502,
+        'https://paypal.wd1.myworkdayjobs.com/wday/cxs/paypal/jobs/jobs', payload)
+    lock.acquire()
+    writeToExcel('Paypal Company', jobsDictionary, worksheet,cell_format)
+    lock.release()
+
+def intelComp(lock,worksheet,cell_format):
+    payload = {"appliedFacets":{},"limit":10,"offset":0,"searchText":""}
+    jobsDictionary = initComp(
+        'https://intel.wd1.myworkdayjobs.com/External',
+        472, 508,
+        'https://intel.wd1.myworkdayjobs.com/wday/cxs/intel/External/jobs', payload)
+    lock.acquire()
+    writeToExcel('Intel Company', jobsDictionary, worksheet,cell_format)
+    lock.release()
+
+def bakerhughesComp(lock,worksheet,cell_format):
+    payload = {"appliedFacets":{"jobFamilyGroup":["5ec015e5642301a63ebe325f4b504d16"],"locationHierarchy1":["c4f78be1a8f14da0ab49ce1162348a5e"]},"limit":10,"offset":0,"searchText":""}
+    jobsDictionary = initComp(
+        'https://bakerhughes.wd5.myworkdayjobs.com/BakerHughes?jobFamilyGroup=5ec015e5642301a63ebe325f4b504d16&locationHierarchy1=c4f78be1a8f14da0ab49ce1162348a5e',
+        671, 707,
+        'https://bakerhughes.wd5.myworkdayjobs.com/wday/cxs/bakerhughes/BakerHughes/jobs', payload)
+    lock.acquire()
+    writeToExcel('Baker Hughes Company', jobsDictionary, worksheet,cell_format)
+    lock.release()
+
+def yahooComp(lock,worksheet,cell_format):
+    payload = {"appliedFacets":{"jobFamilyGroup":["91f14896cbbe0142bf25083fc74637b2","91f14896cbbe0150163e1d3fc7463fb2"]},"limit":10,"offset":0,"searchText":""}
+    jobsDictionary = initComp(
+        'https://ouryahoo.wd5.myworkdayjobs.com/careers?jobFamilyGroup=91f14896cbbe0142bf25083fc74637b2&jobFamilyGroup=91f14896cbbe0150163e1d3fc7463fb2',
+        476, 512,
+        'https://ouryahoo.wd5.myworkdayjobs.com/wday/cxs/ouryahoo/careers/jobs', payload)
+    lock.acquire()
+    writeToExcel('Yahoo Company', jobsDictionary, worksheet,cell_format)
+    lock.release()
+
+def mcafeeComp(lock,worksheet,cell_format):
+    payload = {"appliedFacets":{},"limit":10,"offset":0,"searchText":""}
+    jobsDictionary = initComp(
+        'https://mcafee.wd1.myworkdayjobs.com/External/',474 ,510, 'https://mcafee.wd1.myworkdayjobs.com/wday/cxs/mcafee/External/jobs', payload)
+    lock.acquire()
+    writeToExcel('Mcafee Company', jobsDictionary, worksheet, cell_format)
     lock.release()
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import xlsxwriter
-from companyProcessing import qualcommComp,cienaComp,adobeComp
+from companyProcessing import qualcommComp,cienaComp,adobeComp,paypalComp,intelComp,bakerhughesComp,yahooComp,mcafeeComp
 import threading
 
 # Workbook() takes one, non-optional, argument
 # which is the filename that we want to create.
 workbook = xlsxwriter.Workbook('JobPostings.xlsx')
-
+cell_format = workbook.add_format()
+cell_format.set_bold()      # Turns bold on.
+cell_format.set_bold(True)  # Also turns bold on.
 # The workbook object is then used to add new
 # worksheet via the add_worksheet() method.
 worksheet = workbook.add_worksheet()
@@ -13,17 +15,31 @@ worksheet = workbook.add_worksheet()
 lock = threading.Lock()
 
 # creating threads
-t1 = threading.Thread(target=cienaComp, args=(lock,worksheet))
-t2 = threading.Thread(target=adobeComp, args=(lock,worksheet))
-t3 = threading.Thread(target=qualcommComp, args=(lock,worksheet))
-
+t1 = threading.Thread(target=cienaComp, args=(lock,worksheet,cell_format))
+t2 = threading.Thread(target=adobeComp, args=(lock,worksheet,cell_format))
+t3 = threading.Thread(target=qualcommComp, args=(lock,worksheet,cell_format))
+t4 = threading.Thread(target=paypalComp, args=(lock,worksheet,cell_format))
+t5 = threading.Thread(target=intelComp, args=(lock,worksheet,cell_format))
+t6 = threading.Thread(target=bakerhughesComp, args=(lock,worksheet,cell_format))
+t7 = threading.Thread(target=yahooComp, args=(lock,worksheet,cell_format))
+t8 = threading.Thread(target=mcafeeComp, args=(lock,worksheet,cell_format))
 # start threads
 t1.start()
 t2.start()
 t3.start()
+t4.start()
+t5.start()
+t6.start()
+t7.start()
+t8.start()
 # wait until threads finish their job
 t1.join()
 t2.join()
 t3.join()
+t4.join()
+t5.join()
+t6.join()
+t7.join()
+t8.join()
 
 workbook.close()
