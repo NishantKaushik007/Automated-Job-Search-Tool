8a380f6e091aa13b78b81e2047515a22b2a3bac6
NishantKaushik007 <nishant.nishantkaushik.nishant@gmail.com>
2023-07-01 13:02:38 +0530

    Threading Code Optimized

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import xlsxwriter
-from companyProcessing import qualcommComp,cienaComp,adobeComp,paypalComp,intelComp,bakerhughesComp,yahooComp,mcafeeComp
 import threading
-
+import companyProcessing
 # Workbook() takes one, non-optional, argument
 # which is the filename that we want to create.
 workbook = xlsxwriter.Workbook('JobPostings.xlsx')
@@ -15,31 +14,16 @@ worksheet = workbook.add_worksheet()
 lock = threading.Lock()
 
 # creating threads
-t1 = threading.Thread(target=cienaComp, args=(lock,worksheet,cell_format))
-t2 = threading.Thread(target=adobeComp, args=(lock,worksheet,cell_format))
-t3 = threading.Thread(target=qualcommComp, args=(lock,worksheet,cell_format))
-t4 = threading.Thread(target=paypalComp, args=(lock,worksheet,cell_format))
-t5 = threading.Thread(target=intelComp, args=(lock,worksheet,cell_format))
-t6 = threading.Thread(target=bakerhughesComp, args=(lock,worksheet,cell_format))
-t7 = threading.Thread(target=yahooComp, args=(lock,worksheet,cell_format))
-t8 = threading.Thread(target=mcafeeComp, args=(lock,worksheet,cell_format))
-# start threads
-t1.start()
-t2.start()
-t3.start()
-t4.start()
-t5.start()
-t6.start()
-t7.start()
-t8.start()
-# wait until threads finish their job
-t1.join()
-t2.join()
-t3.join()
-t4.join()
-t5.join()
-t6.join()
-t7.join()
-t8.join()
+companies = ["ciena","adobe","qualcomm","paypal","intel","bakerhughes","yahoo","mcafee"]
+threads = []
+for i in companies:
+    t = threading.Thread(target=getattr(companyProcessing,i+'Comp'), args=(lock,worksheet,cell_format))
+    threads.append(t)
+
+for i in threads:
+    i.start()
+
+for i in threads:
+    i.join()
 
 workbook.close()
